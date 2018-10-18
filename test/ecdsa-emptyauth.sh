2cc72e5fb58a14bc284f1e90df6a44aafc75d8ab
Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>
2018-10-18 12:03:56 +0200

    Add option for password-less operations
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

---
+++ test/ecdsa-emptyauth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eufx
+
+export LANG=C
+export OPENSSL_ENGINES=${PWD}/.libs
+export PATH=${PWD}:${PATH}
+
+DIR=$(mktemp -d)
+echo -n "abcde12345abcde12345">${DIR}/mydata
+tpm2tss-genkey -a ecdsa -c nist_p256 ${DIR}/mykey
+
+openssl pkeyutl -keyform engine -engine tpm2tss -inkey ${DIR}/mykey -sign -in ${DIR}/mydata -out ${DIR}/mysig
+
+R="$(openssl pkeyutl -keyform engine -engine tpm2tss -inkey ${DIR}/mykey -verify -in ${DIR}/mydata -sigfile ${DIR}/mysig || true)"
+if ! echo $R | grep "Signature Verified Successfully" >/dev/null; then
+    echo $R
+    exit 1
+fi
