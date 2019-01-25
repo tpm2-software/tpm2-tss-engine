c7bae082dfdc2985f920c46959266b05162f26a0
GaelGuegan <gaelguegan1995@gmail.com>
2019-01-25 16:08:11 +0100

    Added cipher structure : EVP_CIPHER tpm_aes256_cbc{}
    Signed-off-by: GaelGuegan <gaelguegan1995@gmail.com>
    Signed-off-by: GaelGuegan <gael.guegan@non.se.com>

--- src/tpm2-tss-engine-ciphers.c
+++ src/tpm2-tss-engine-ciphers.c
@@ -11,68 +11,77 @@ https://github.com/gost-engine/engine/blob/master/gost_eng.c
 #include "tpm2-tss-engine.h"
 #include "tpm2-tss-engine-common.h"
 
-static int tpm_aes_nids[] = {NID_aes_256_cbc};
+static int tpm_aes_nids[] = {NID_aes256_cbc};
+
+/*
+struct evp_cipher_ctx_st {
+    const EVP_CIPHER *cipher;
+    ENGINE *engine;             /* functional reference if 'cipher' is
+                                 * ENGINE-provided
+    int encrypt;                /* encrypt or decrypt
+    int buf_len;                /* number we have left
+    unsigned char oiv[EVP_MAX_IV_LENGTH]; /* original iv
+    unsigned char iv[EVP_MAX_IV_LENGTH]; /* working iv
+    unsigned char buf[EVP_MAX_BLOCK_LENGTH]; /* saved partial block
+    int num;                    /* used by cfb/ofb/ctr mode
+    FIXME: Should this even exist? It appears unused
+    void *app_data;             /* application stuff
+    int key_len;                /* May change for variable length cipher
+    unsigned long flags;        /* Various flags
+    void *cipher_data;          /* per EVP data
+    int final_used;
+    int block_mask;
+    unsigned char final[EVP_MAX_BLOCK_LENGTH]; /* possible final block
+}  EVP_CIPHER_CTX  ;
+*/
 
 static int
-cipher_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc)
+tpm_cipher_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc)
 {
+    EVP_CIPHER_CTX_get_app_data
+    ctx->cipher->app_data
+
     TSS2_RC r;
     ESYS_AUXCONTEXT eactx = (ESYS_AUXCONTEXT){0};
     ESYS_TR keyHandle = ESYS_TR_NONE;
     TPMT_SIGNATURE *sig = NULL;
 
-    r = init_tpm_key (  &eactx,
-                        &keyHandle,
-                        tpm2Data);
-    ERRchktss(cipher_init_key, r, goto error);
+    r = init_tpm_key(&eactx, &keyHandle, tpm2Data);
+    ERRchktss(tpm_cipher_init_key, r, goto error);
 
 }
 
 static int
-cipher_update(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc)
+tpm_do_cipher_aes256_cbc(EVP_CIPHER_CTX *ctx,unsigned char *out, const unsigned char *in, size_t inl)
 {
-
-
+    //tpm2_encryptdecrypt
 }
 
 static int
-cipher_cleanup(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc)
+tpm_cipher_cleanup(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc)
 {
-
-
+    //cleanup
 }
 
-EVP_CIPHER tpm_ciphers =
+EVP_CIPHER tpm_aes256_cbc =
 {
-    NID_aes_256_cbc,
-    int block_size;
-    /* Default value for variable length ciphers */
-    int key_len;
-    int iv_len;
-    /* Various flags */
-    unsigned long flags;
-    /* init key */
-    int (*init) (EVP_CIPHER_CTX *ctx, const unsigned char *key,
-                 const unsigned char *iv, int enc);
-    /* encrypt/decrypt data */
-    int (*do_cipher) (EVP_CIPHER_CTX *ctx, unsigned char *out,
-                      const unsigned char *in, size_t inl);
-    /* cleanup ctx */
-    int (*cleanup) (EVP_CIPHER_CTX *);
-    /* how big ctx->cipher_data needs to be */
-    int ctx_size;
-    /* Populate a ASN1_TYPE with parameters */
-    int (*set_asn1_parameters) (EVP_CIPHER_CTX *, ASN1_TYPE *);
-    /* Get parameters from a ASN1_TYPE */
-    int (*get_asn1_parameters) (EVP_CIPHER_CTX *, ASN1_TYPE *);
-    /* Miscellaneous operations */
-    int (*ctrl) (EVP_CIPHER_CTX *, int type, int arg, void *ptr);
-    /* Application data */
-    void *app_data;
+    NID_aes256_cbc,                                               // int nid                   // EVP_CIPHER_meth_new()
+    1,                                                            // int block_size;
+    32,                                                           // int key_len;
+    8,                                                            // int iv_len;               // EVP_CIPHER_meth_set_iv_length()
+    EVP_CIPH_CBC_MODE,                                            // unsigned long flags;      // EVP_CIPHER_meth_set_flags()
+    cipher_init_key,                                              // *init_key                 // EVP_CIPHER_meth_set_init()
+    cipher_update,                                                // *do_cipher                // EVP_CIPHER_meth_set_do_cipher()
+    cipher_cleanup,                                               // cleanup ctx               // EVP_CIPHER_meth_set_cleanup()
+    1000,                                                         // int cipher_data size      // EVP_CIPHER_meth_set_impl_ctx_size()
+    EVP_CIPHER_set_asn1_iv,                                       //                           // EVP_CIPHER_meth_set_set_asn1_params
+    EVP_CIPHER_set_asn1_iv,                                       //                           // EVP_CIPHER_meth_set_get_asn1_params
+    NULL,                                                         //                           // EVP_CIPHER_meth_set_ctrl
+    NULL                                                          // void *app_data;
 }
 
 static int
-tpm_ciphers(ENGINE* e, const EVP_CIPHER** cipher, const int** nids, int nid)
+tpm_ciphers_selector(ENGINE* e, const EVP_CIPHER** cipher, const int** nids, int nid)
 {
     if (cipher == NULL) {
         *nids = tpm_nids;
@@ -93,7 +102,7 @@ tpm_ciphers(ENGINE* e, const EVP_CIPHER** cipher, const int** nids, int nid)
 int
 init_ciphers(ENGINE *e)
 {
-    if ( !ENGINE_set_ciphers(e, tpm_ciphers)) {
+    if ( !ENGINE_set_ciphers(e, tpm_ciphers_selector)) {
         printf("ENGINE_set_ciphers failed\n");
         return 0;
     }
