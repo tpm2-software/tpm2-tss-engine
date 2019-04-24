4b091b1c08894cce1df054ba468c812c92d5c682
Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>
2019-04-24 12:35:08 +0200

    Tests: Use long parameters for tpm2-tools
    Since they have proven to be more stable, start using long-form
    parameters for the tpm2-tools usages.
    
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

--- test/rsasign_persistent_emptyauth.sh
+++ test/rsasign_persistent_emptyauth.sh
@@ -8,23 +8,29 @@ echo -n "abcde12345abcde12345">mydata.txt
 echo "Generating primary key"
 PARENT_CTX=primary_owner_key.ctx
 
-tpm2_createprimary -a o -g sha256 -G rsa -o ${PARENT_CTX}
-tpm2_flushcontext -t
+tpm2_createprimary --hierarchy=o --halg=sha256 --kalg=rsa \
+                   --out-context-name=${PARENT_CTX}
+tpm2_flushcontext --transient-object
 
 # Create an RSA key pair
 echo "Generating RSA key pair"
 TPM_RSA_PUBKEY=rsakey.pub
 TPM_RSA_KEY=rsakey
-tpm2_create -C ${PARENT_CTX} -g sha256 -G rsa -u ${TPM_RSA_PUBKEY} -r ${TPM_RSA_KEY} -b sign\|decrypt\|fixedtpm\|fixedparent\|sensitivedataorigin\|userwithauth\|noda
-tpm2_flushcontext -t
+tpm2_create --context-parent=${PARENT_CTX} \
+            --halg=sha256 --kalg=rsa \
+            --pubfile=${TPM_RSA_PUBKEY} --privfile=${TPM_RSA_KEY} \
+            --object-attributes=sign\|decrypt\|fixedtpm\|fixedparent\|sensitivedataorigin\|userwithauth\|noda
+tpm2_flushcontext --transient-object
 
 # Load Key to persistent handle
 RSA_CTX=rsakey.ctx
-tpm2_load -C ${PARENT_CTX} -u ${TPM_RSA_PUBKEY} -r ${TPM_RSA_KEY} -o ${RSA_CTX}
-tpm2_flushcontext -t
+tpm2_load --context-parent=${PARENT_CTX} \
+          --pubfile=${TPM_RSA_PUBKEY} --privfile=${TPM_RSA_KEY} \
+          --out-context=${RSA_CTX}
+tpm2_flushcontext --transient-object
 
-HANDLE=$(tpm2_evictcontrol -a o -c ${RSA_CTX} | cut -d ' ' -f 2)
-tpm2_flushcontext -t
+HANDLE=$(tpm2_evictcontrol --hierarchy=o --context=${RSA_CTX} | cut -d ' ' -f 2)
+tpm2_flushcontext --transient-object
 
 # Signing Data
 #Actually signing should not require an auth value
@@ -39,10 +45,10 @@ EOF
 fi
 
 # Get public key of handle
-tpm2_readpublic -c ${HANDLE} -o mykey.pem -f pem
+tpm2_readpublic --context=${HANDLE} --out-file=mykey.pem --format=pem
 
 # Release persistent HANDLE
-tpm2_evictcontrol -a o -c ${HANDLE} -p ${HANDLE}
+tpm2_evictcontrol --hierarchy=o --context=${HANDLE} --persistent=${HANDLE}
 
 R="$(openssl pkeyutl -pubin -inkey mykey.pem -verify -in mydata.txt -sigfile mysig || true)"
 if ! echo $R | grep "Signature Verified Successfully" >/dev/null; then
