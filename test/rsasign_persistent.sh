164590ffa0b08f5ef6fcd5de55f81dd2f6be92ec
Jonas Witschel <diabonas@gmx.de>
2019-08-25 00:18:03 +0200

    test: use tpm2-tools 4.X
    Since tpm2-tools 4.0 has been released, we can update our tests.
    tpm2-tools 4.0 also requires tpm2-tss 2.3, so bump that as well.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- test/rsasign_persistent.sh
+++ test/rsasign_persistent.sh
@@ -8,38 +8,38 @@ echo -n "abcde12345abcde12345">mydata.txt
 echo "Generating primary key"
 PARENT_CTX=primary_owner_key.ctx
 
-tpm2_createprimary --hierarchy=o --halg=sha256 --kalg=rsa \
-                   --context=${PARENT_CTX}
+tpm2_createprimary --hierarchy=o --hash-algorithm=sha256 --key-algorithm=rsa \
+                   --key-context=${PARENT_CTX}
 tpm2_flushcontext --transient-object
 
 # Create an RSA key pair
 echo "Generating RSA key pair"
 TPM_RSA_PUBKEY=rsakey.pub
 TPM_RSA_KEY=rsakey
-tpm2_create --pwdk=abc \
-            --context-parent=${PARENT_CTX} \
-            --halg=sha256 --kalg=rsa \
-            --pubfile=${TPM_RSA_PUBKEY} --privfile=${TPM_RSA_KEY} \
-            --object-attributes=sign\|decrypt\|fixedtpm\|fixedparent\|sensitivedataorigin\|userwithauth\|noda
+tpm2_create --key-auth=abc \
+            --parent-context=${PARENT_CTX} \
+            --hash-algorithm=sha256 --key-algorithm=rsa \
+            --public=${TPM_RSA_PUBKEY} --private=${TPM_RSA_KEY} \
+            --attributes=sign\|decrypt\|fixedtpm\|fixedparent\|sensitivedataorigin\|userwithauth\|noda
 tpm2_flushcontext --transient-object
 
 # Load Key to persistent handle
 RSA_CTX=rsakey.ctx
-tpm2_load --context-parent=${PARENT_CTX} \
-          --pubfile=${TPM_RSA_PUBKEY} --privfile=${TPM_RSA_KEY} \
-          --context=${RSA_CTX}
+tpm2_load --parent-context=${PARENT_CTX} \
+          --public=${TPM_RSA_PUBKEY} --private=${TPM_RSA_KEY} \
+          --key-context=${RSA_CTX}
 tpm2_flushcontext --transient-object
 
-HANDLE=$(tpm2_evictcontrol --auth=o --context=${RSA_CTX} --persistent=0x81010001 | cut -d ' ' -f 2 | head -n 1)
+HANDLE=$(tpm2_evictcontrol --hierarchy=o --object-context=${RSA_CTX} | cut -d ' ' -f 2 | head -n 1)
 tpm2_flushcontext --transient-object
 
 # Signing Data
 echo "abc" | openssl pkeyutl -engine tpm2tss -keyform engine -inkey ${HANDLE} -sign -in mydata.txt -out mysig -passin stdin
 # Get public key of handle
-tpm2_readpublic --object=${HANDLE} --opu=mykey.pem --format=pem
+tpm2_readpublic --object-context=${HANDLE} --output=mykey.pem --format=pem
 
 # Release persistent HANDLE
-tpm2_evictcontrol --auth=o --handle=${HANDLE} --persistent=${HANDLE}
+tpm2_evictcontrol --hierarchy=o --object-context=${HANDLE}
 
 R="$(openssl pkeyutl -pubin -inkey mykey.pem -verify -in mydata.txt -sigfile mysig || true)"
 if ! echo $R | grep "Signature Verified Successfully" >/dev/null; then
