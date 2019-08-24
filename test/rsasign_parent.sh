46074881bd8adba96eb35d9e630db9988ea9259d
Jonas Witschel <diabonas@gmx.de>
2019-08-25 00:18:03 +0200

    test: use tpm2-tools 4.X
    Since tpm2-tools 4.0 has been released, we can update our tests.
    tpm2-tools 4.0 also requires tpm2-tss 2.3, so bump that as well.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- test/rsasign_parent.sh
+++ test/rsasign_parent.sh
@@ -8,12 +8,12 @@ echo -n "abcde12345abcde12345">mydata.txt
 echo "Generating primary key"
 PARENT_CTX=primary_owner_key.ctx
 
-tpm2_createprimary --hierarchy=o --halg=sha256 --kalg=rsa \
-                   --context=${PARENT_CTX}
+tpm2_createprimary --hierarchy=o --hash-algorithm=sha256 --key-algorithm=rsa \
+                   --key-context=${PARENT_CTX}
 tpm2_flushcontext --transient-object
 
 # Load primary key to persistent handle
-HANDLE=$(tpm2_evictcontrol --auth=o --context=${PARENT_CTX} --persistent=0x81010001 | cut -d ' ' -f 2 | head -n 1)
+HANDLE=$(tpm2_evictcontrol --hierarchy=o --object-context=${PARENT_CTX} | cut -d ' ' -f 2 | head -n 1)
 tpm2_flushcontext --transient-object
 
 # Generating a key underneath the persistent parent
@@ -25,7 +25,7 @@ cat mykey.pub
 echo "abc" | openssl pkeyutl -engine tpm2tss -keyform engine -inkey mykey -sign -in mydata.txt -out mysig -passin stdin
 
 # Release persistent HANDLE
-tpm2_evictcontrol --auth=o --handle=${HANDLE} --persistent=${HANDLE}
+tpm2_evictcontrol --hierarchy=o --object-context=${HANDLE}
 
 #this is a workaround because -verify allways exits 1
 R="$(openssl pkeyutl -pubin -inkey mykey.pub -verify -in mydata.txt -sigfile mysig || true)"
