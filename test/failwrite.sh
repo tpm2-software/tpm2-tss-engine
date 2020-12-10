8f026be3be0440a14d4f07cbcb60f3a7b2821752
William Roberts <william.c.roberts@intel.com>
2020-12-10 09:57:29 -0600

    test/failwrite.sh: use a bad path for testing
    Rather than rely on CAP_DAC_OVERRIDE being unset, and thus runs with
    user root (assuming root has CAP_DAC_OVERRIDE) to fail, use a bad path
    so failure occurs without respect to caps.
    
    This fixes build/tests as user root when running in a docker env.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/failwrite.sh
+++ test/failwrite.sh
@@ -2,10 +2,7 @@
 
 set -eufx
 
-echo -n "abcde12345abcde12345">mykey
-chmod ugo-rwx mykey
-
-R="$(tpm2tss-genkey -a ecdsa -c nist_p256 -p abc mykey 2>&1 || true)"
+R="$(tpm2tss-genkey -a ecdsa -c nist_p256 -p abc /no/such/file/path 2>&1 || true)"
 echo $R
 if ! echo $R | grep "Error writing file" >/dev/null; then
     exit 1
